21766ab318b6997396462720a56fd272102a4bf1
CindyKE <cindyke124@outlook.com>
2023-12-13 23:14:26 -0500

    Dec13， 11:14pm

--- SHEMS/settings.py
+++ SHEMS/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'SHEMS',
         'USER': 'root',
-        'PASSWORD': '1203',
+        'PASSWORD': 'root123456789',
         'HOST': '127.0.0.1',
         'PORT': 3306
 
