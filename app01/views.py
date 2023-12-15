6d454c6059e3eb4b744e2ed66d3febfc904502ec
CindyKE <cindyke124@outlook.com>
2023-12-15 00:32:56 -0500

    Dec15， 12:32 am

--- app01/views.py
+++ app01/views.py
@@ -2,6 +2,11 @@ from django.urls import reverse
 
 from django.shortcuts import render, HttpResponse, redirect
 from django.db import connection
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import base64
+from io import BytesIO
 
 
 # Create your views here.
@@ -29,7 +34,6 @@ def customer_login(request):
 
 
 def register_page(request):
-    print("method:", request.method)
     if request.method == "GET":
         return render(request, "register_page.html")
 
@@ -49,10 +53,6 @@ def register_page(request):
 
 def register_success(request, new_id):
     return render(request, "register_success.html", {"customer_id": new_id})
-
-
-def energy_consumption(request, customer_id):
-    return render(request, "energy_consumption.html", {"customer_id": customer_id})
 
 
 def account_management(request, customer_id):
@@ -128,7 +128,7 @@ def list_devices(request):
     customer_id = request.POST.get("customer_id") or request.GET.get("customer_id")
     with connection.cursor() as cursor:
         cursor.execute("""
-            SELECT ed.device_id, dt.type_name, dm.model_number, ed.enrollment_date
+            SELECT ed.device_id, sl.location_id, sl.service_address, dt.type_name, dm.model_number, ed.enrollment_date
             FROM EnrolledDevices ed
             INNER JOIN DeviceModels dm ON ed.model_id = dm.model_id
             INNER JOIN DeviceTypes dt ON dm.type_id = dt.type_id
@@ -150,9 +150,11 @@ def delete_devices(request):
         cursor.execute("DELETE FROM EnrolledDevices WHERE device_id = %s", [device_id])
     redirect_url = '/list_devices/' + f'?customer_id={customer_id}'
     return redirect(redirect_url)
+    # return render(request, "list_devices.html", {"customer_id": customer_id})
 
 
 def add_devices(request):
+
     customer_id = request.POST.get("customer_id")
     if request.method == 'POST':
         device_id = request.POST.get("device_id")
@@ -173,3 +175,103 @@ def add_devices(request):
 
     redirect_url = '/list_devices/' + f'?customer_id={customer_id}'
     return redirect(redirect_url)
+
+
+def energy_consumption(request, customer_id):
+    if request.method == "GET":
+        # location_ids = []
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT DISTINCT location_id FROM CustomerServices WHERE customer_id = %s ORDER BY location_id ASC",[customer_id])
+            location_ids = cursor.fetchall()
+
+        with connection.cursor() as cursor:
+            cursor.execute("""
+            SELECT DISTINCT device_id 
+            FROM CustomerServices cs JOIN EnrolledDevices ed ON cs.location_id = ed.location_id
+            WHERE customer_id = %s
+            ORDER BY device_id ASC
+            """, [customer_id])
+
+            device_ids = cursor.fetchall()
+
+        return render(request, "energy_consumption.html", {"customer_id": customer_id, "location_ids": location_ids, "device_ids": device_ids})
+
+    if request.POST.get("choice") == "Daily Consumption Check":
+        location_id = request.POST.get("location_id")
+        start_time_str = request.POST.get("start_time")
+        end_time_str = request.POST.get("end_time")
+        all_consumption = daily_consumption_check(location_id,start_time_str,end_time_str)
+        dates = []
+        kwh_totals = []
+        for row in all_consumption:
+            dates.append(row[0])
+            kwh_totals.append(row[1])
+            print(row[0]," ", row[1])
+        graphic1 = daily_consumption_graphic(dates,kwh_totals)
+        return render(request, "energy_consumption.html", {"customer_id": customer_id, 'graphic1': graphic1})
+
+    elif request.POST.get("choice") == "Device Consumption Check":
+        device_id = request.POST.get("device_id")
+        start_time_str = request.POST.get("start_time")
+        end_time_str = request.POST.get("end_time")
+        device_consumption = device_daily_consumption(device_id, start_time_str, end_time_str)
+        dates = []
+        kwh_totals = []
+        for row in device_consumption:
+            dates.append(row[0])
+            kwh_totals.append(row[1])
+            print(row[0], " ", row[1])
+        graphic2 = daily_consumption_graphic(dates, kwh_totals)
+        return render(request, "energy_consumption.html", {"customer_id": customer_id, 'graphic2': graphic2})
+
+
+def daily_consumption_check(location_id, start_time_str, end_time_str):
+    start_time = datetime.strptime(start_time_str, '%Y-%m-%dT%H:%M')
+    end_time = datetime.strptime(end_time_str, '%Y-%m-%dT%H:%M')
+    formatted_start_time = start_time.strftime('%Y-%m-%d %H:%M:%S')
+    formatted_end_time = end_time.strftime('%Y-%m-%d %H:%M:%S')
+    with connection.cursor() as cursor:
+        cursor.execute("""
+                SELECT DATE(time_stamp) AS date, SUM(kWh) AS total_kWh
+                FROM EnergyUsage eu 
+                JOIN EnrolledDevices ed ON eu.device_id = ed.device_id
+                WHERE location_id = %s AND time_stamp BETWEEN %s AND %s
+                GROUP BY DATE(time_stamp)
+                ORDER BY DATE(time_stamp) ASC
+            """, [location_id, formatted_start_time, formatted_end_time])
+        return cursor.fetchall()
+
+def device_daily_consumption(device_id,start_time_str, end_time_str):
+    start_time = datetime.strptime(start_time_str, '%Y-%m-%dT%H:%M')
+    end_time = datetime.strptime(end_time_str, '%Y-%m-%dT%H:%M')
+    formatted_start_time = start_time.strftime('%Y-%m-%d %H:%M:%S')
+    formatted_end_time = end_time.strftime('%Y-%m-%d %H:%M:%S')
+    with connection.cursor() as cursor:
+        cursor.execute("""
+                SELECT DATE(time_stamp) AS date, SUM(kWh) AS total_kWh
+                FROM EnergyUsage
+                WHERE device_id = %s AND time_stamp BETWEEN %s AND %s
+                GROUP BY DATE(time_stamp)
+                ORDER BY DATE(time_stamp) ASC
+            """, [device_id, formatted_start_time, formatted_end_time])
+        return cursor.fetchall()
+
+def daily_consumption_graphic(dates, kwh_totals):
+    # dates = [datetime.strptime(date, '%Y-%m-%d') for date in dates]
+    plt.figure(figsize=(10, 6))  # 设置图形大小
+    plt.plot(dates, kwh_totals, marker='o')  # 创建折线图
+    plt.xticks(rotation=45, ha='right')  # 旋转标签并对齐
+    plt.xlabel('Date')
+    plt.ylabel('Total kWh')
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+
+    plt.tight_layout()
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png', bbox_inches='tight')
+    plt.close()
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    buffer.close()
+    graphic1 = base64.b64encode(image_png).decode('utf-8')
+
+    return graphic1
